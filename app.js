000f91ba895af8e1100c84104e32804d9f05b296
saiemsaeed <33630921+saiemsaeed@users.noreply.github.com>
2018-03-23 15:56:34 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now} : ${req.method} - ${req.path} \n`;
+    fs.appendFileSync("server.log", log);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('contact');
+});
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('streamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: "Home Page",
+        welcomeMessage: "Welcome to My Website!"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: "Saiem's Page",
+        text: "I am Saiem Saeed, Son of Muhammad Saeed and Shakeela Saeed!"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening to port 3000!");
+})
